6f9d2af560e45792c903d7fccb91223651cf7b85
ALUNO SENAI PETROLINA <helio96011c2@gmail.com>
2025-10-16 09:30:15 -0300

    atividade

---
+++ funcoes.dart
@@ -0,0 +1,11 @@
+void saudacao(String nome) {
+   print ("ola, $nome!");
+    
+    }
+
+void main(){
+  saudacao("maria");
+  saudacao("joão");
+
+
+}
---
+++ funcoes1.dart
@@ -0,0 +1,23 @@
+soma (double a, double b){
+  return a+b;
+
+}
+
+double divisao (double a,double b){
+  return a/b; 
+}
+
+double multiplicacao (double a, double b){
+  return a*b;
+
+
+}
+
+void main (){
+  print (soma (34,33));
+
+  print(divisao(134, 2));
+
+  print(multiplicacao(33.5, 2));
+
+}
